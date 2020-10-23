9cb3ad52c13557e51484430cb4360fa118c89ab2
anarita_cavalheiro <anarita.cavalheiro@hotmail.com>
2020-10-23 14:41:06 +0100

    done

--- js/car.js
+++ js/car.js
@@ -1,32 +1,33 @@
 class Car {
-    constructor() {
-        this.x = 220;
-        this.y = 520;
-        this.width = 50;
-        this.height = 80;
-        this.imagSrc = './image/car.png';
+    constructor(){
+      this.x = 220;
+      this.y = 520;
+      this.width = 50;
+      this.height = 80;
+      this.img = './images/car.png';
     }
-    drawCar() { //passes an actual image and not just the src
-        const carIma = new Image();
-        carIma = this.imagSrc;
-        Ctx.drawImg(carIma, this.x, this.y, this.width, this.height); //ctx already exists in the first fiel so it's already loaded
+    drawCar(){
+      const carImg = new Image();
+      carImg.src = this.img;
+      ctx.drawImage(carImg, this.x, this.y, this.width, this.height);
     }
-
-    moveCar() { //coordenites mean we are just cleaning the car
-        ctx.clearRect(this.x, this.y, this.width, this.height);
-        switch (keyCode) { 
-            //move to the left
-            case 37:
-                if(this.x > 20) { //making sure the car doesn's go off road
-                    this.x -= 10;
-                }
-            break;
-            //move to the right
-            case 39:
-                if(this.x < 430) {
-                 this.x += 10;   
-                }
-            break;    
+    moveCar(keyCode){
+      console.log('x', this.x);
+      console.log('y', this.y);
+      ctx.clearRect(this.x, this.y, this.width, this.height);
+      switch(keyCode){
+        case 37:
+        //Making sure car doesn't go off the road
+        if(this.x > 20){
+          this.x -= 10;
         }
+          break;
+        case 39:
+        //Making sure car doesn't go off the road
+        if (this.x < 430 ){
+          this.x += 10;
+        }
+          break;
+      }
     }
-}
+  }
--- js/index.js
+++ js/index.js
@@ -108,77 +108,69 @@ window.onload = () => {
   };
 
   let currentGame;
-  let currentCar;
-  document.getElementById('game-board').style.display = 'none';
-  const myCanvas = document.getElementById('the-canvas');
-  const ctx = myCanvas.getContext('2d');
-  document.getElementById('start-button').onclick = () => {
-      startGame();
-  }
-  document.onkeydown = (e) => {
-      let whereToGo = e.keyCode;
-      currentGame.car.moveCar(whereToGo);
-  }
-  function startGame() {
-      document.getElementById('game-board').style.display = 'block';
-      //Instantiate a new game of the game class
-      currentGame = new Game();
-      //Instantiate a new car
-      currentCar = new Car();
-      currentGame.car = currentCar;
-      currentCar.drawCar();
-      updateCanvas();
-  }
-
-  let obstaclesFequency = 0;
-
-  function updateCanvas() {
+let currentCar;
+document.getElementById('game-board').style.display = 'none';
+const myCanvas = document.getElementById('the-canvas');
+const ctx = myCanvas.getContext('2d');
+document.getElementById('start-button').onclick = () => {
+    startGame();
+}
+document.onkeydown = (e) => {
+    let whereToGo = e.keyCode;
+    currentGame.car.moveCar(whereToGo);
+}
+function startGame() {
+    document.getElementById('game-board').style.display = 'block';
+    //Instantiate a new game of the game class
+    currentGame = new Game();
+    //Instantiate a new car
+    currentCar = new Car();
+    currentGame.car = currentCar;
+    currentGame.car.drawCar();
+    updateCanvas();
+}
+function detectCollision(obstacle) {
+    return !((currentCar.y > obstacle.y + obstacle.height) || 
+    (currentCar.x + currentCar.width < obstacle.x) || 
+    (currentCar.x - currentCar.width  > obstacle.x + obstacle.width))
+}
+let obstaclesFrequency = 0;
+function updateCanvas() {
     ctx.clearRect(0, 0, 500, 600);
-      currentCar.drawCar();
-      frames ++;
-      if(obstaclesFequency % 100 === 1) {
-//draw obsticle
-      let randomObstacleX = Math.floor(Math.random() * 450);
-      let randomObstacleY = 0;
-      let randomObstacleWidth = Math.floor(Math.random() * 50) + 20;
-      let randomObstacleHeight = Math.floor(Math.random() * 50) + 20;
-      let newObstacle = new Obstacle(randomObstacleX, randomObstacleY, randomObstacleWidth, randomObstacleHeight);
-      }
-
-      function detectCollision(obstacle) {
-        return !((currentCar.y > obstacle.y + obstacle.height) ||
-        (currentCar.x + currentCar.width < obstacle.x) ||
-        (currentCar.x - currentCar.width > obstacle.x + obstacle.width))
-      }
-
-      currentGame.obstacle.push(newObstacle)
-      //console.log(currentGame.obstacles);
-      for(let i=0; i<obstacles.length; i++) {
+    currentGame.car.drawCar();
+    obstaclesFrequency++;
+    if (obstaclesFrequency % 100 === 1) {
+        //Draw an obstacle
+        let randomObstacleX = Math.floor(Math.random() * 450);
+        let randomObstacleY = 0;
+        let randomObstacleWidth = Math.floor(Math.random() * 50) + 20;
+        let randomObstacleHeight = Math.floor(Math.random() * 50) + 20;
+        let newObstacle = new Obstacle(
+            randomObstacleX, 
+            randomObstacleY, 
+            randomObstacleWidth, 
+            randomObstacleHeight);
+        currentGame.obstacles.push(newObstacle);
+        //console.log(currentGame.obstacles);
+    }
+    for(let i = 0; i<currentGame.obstacles.length; i++) {
         currentGame.obstacles[i].y += 1;
         currentGame.obstacles[i].drawObstacle();
-
-        if(detectCollision(currentGame.obstacle[i])) {
-          alert('BOOOOOM!')
-          obstaclesFequency= 0;
-          currentGame.score= 0;
-          document.getElementById('score').innerHTML = 0;
-          currentGame.obstacle= [];
-          document.getElementById('game-board').style.display= 'none';
+        if (detectCollision(currentGame.obstacles[i])) {
+            alert('BOOOOOMM!')
+            obstaclesFrequency = 0;
+            currentGame.score = 0;
+            document.getElementById('score').innerHTML = 0;
+            currentGame.obstacles = [];
+            document.getElementById('game-board').style.display = 'none';
         }
-// if obsticle moved outside the canvas it gets taken out of the obsticles array
-        if(currentGame.obstacles.length > 0 && currentGame.obstacles[i].y >= 600) {
-          currentGame.obstacles.splice(i,1);
-          currentGame.score++;
-          document.getElementById('score').innerHTML = currentGame.score;
+        // Obstacle moved outside the canvas
+        if (currentGame.obstacles[i].y >= 600) {
+            currentGame.obstacles.splice(i, 1);
+            currentGame.score++;
+            document.getElementById('score').innerHTML = currentGame.score;
         }
-  
-      }
-
-      requestAnimationFrame(updateCanvas);
     }
-  }
-
-     /* setInterval(() => {
-          updateCanvas();
-      }, 60);*/
-  
+    requestAnimationFrame(updateCanvas);
+}
+}
